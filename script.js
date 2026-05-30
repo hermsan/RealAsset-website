cae104dcad27d784c00ef9fc48c13b16d77a51a6
herman <140790460+hermsan@users.noreply.github.com>
2026-05-30 19:05:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openTwitter(){
+window.open("https://x.com/MAGA_Aliens","_blank");
+}
+
+function openTelegram(){
+window.open("https://t.me/usernamekamu","_blank");
+}
+
+function openDiscord(){
+window.open("https://discord.gg/linkserverkamu","_blank");
+}
